cb3b15829e52e2aa95b5632cefc85b0a05a8d22f
mdmhrz <razufreelence@gmail.com>
2025-05-31 20:24:37 +0600

    Single Book Design Done

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -16,14 +16,18 @@ const Books = ({ data }) => {
     // Way-2 for Data Fetching 
     // const bookPromise = fetch('booksData.json').then(res => res.json());
 
+    console.log(data);
+
 
     return (
         <div>
             <h1 className='text-3xl text-center p-6'>Books</h1>
             <Suspense fallback={<span>Loading......</span>}>
-                {
-                    data.map(singleBook => <Book key={singleBook.bookId} data={data}></Book>)
-                }
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                    {
+                        data.map(singleBook => <Book key={singleBook.bookId} singleBook={singleBook}></Book>)
+                    }
+                </div>
             </Suspense>
         </div>
     );
