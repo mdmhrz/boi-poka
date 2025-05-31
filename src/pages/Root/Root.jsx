38d727b6f67ccd3cf08c5309c74e593ef2940c78
mdmhrz <razufreelence@gmail.com>
2025-05-31 17:58:47 +0600

    Navbar & Footer added

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Header/Navbar';
 import { Outlet } from 'react-router';
-import Footer from '../../components/Header/Footer/Footer';
+import Footer from '../../components/Footer/Footer';
 
 const Root = () => {
     return (
